1245ed428e9092532b29f6db085fbe3abaa75612
Vladimir Ljamic <vladimir.ljamic@consolia.rs>
2023-01-15 00:48:07 +0100

    First

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: '.card',
+  templateUrl: './card.component.html'
+})
+export class CardComponent implements OnInit {
+
+  @Input() product: Product;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+export class Product {
+  headline: string;
+  img: string;
+  description: string;
+  url: string;
+}
