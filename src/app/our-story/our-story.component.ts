560063c720c3fc1f746d64100446514fcd52bd2a
Vladimir Ljamic <vladimir.ljamic@consolia.rs>
2023-01-25 16:27:06 +0100

    Front - angular 15

---
+++ src/app/our-story/our-story.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-story',
+  templateUrl: './our-story.component.html'
+})
+export class OurStoryComponent {
+
+}
---
+++ src/app/partnership/partnership.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partnership',
+  templateUrl: './partnership.component.html'
+})
+export class PartnershipComponent {
+
+}
